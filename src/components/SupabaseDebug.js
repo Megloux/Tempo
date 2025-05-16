df7ef9dbe9b4bea099f88b137fdb9147d0f85e9d
Megloux <megbowen.21@icloud.com>
2025-05-16 10:17:31 -0400

    Fix duplicate React import in SupabaseDebug.js

--- src/components/SupabaseDebug.js
+++ src/components/SupabaseDebug.js
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from 'react';
 import React, { useState, useEffect } from 'react';
 import { supabase } from '../supabaseClient';
 import { useClassSchedule } from '../context/ClassScheduleContext';
